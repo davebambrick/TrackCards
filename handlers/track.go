db8e9a59f7cb4f0bd00682771c4a13feb3598f53
davebambrick <dmbambrick@gmail.com>
2017-06-26 13:37:30 -0700

    added documentation for TrackHandler

--- handlers/track.go
+++ handlers/track.go
@@ -11,6 +11,21 @@ import (
 )
 
 func TrackHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+		TrackHandler takes in an HTTP POST request and pulls a json music entity object
+		from the request body. It pulls the entity's type, and then performs a sequence
+		of corresponding Kazaam transformations on the json, before writing the output
+		transformed json to the ResponseWriter.
+
+		Args:
+			w (http.ResponseWriter): ResponseWriter object used to build the response
+			to be sent to the client
+			r (*http.Request): Pointer to the incoming Request object; it is intended
+			to be a POST request so we can access the request body
+
+		Returns:
+			None
+	*/
 
 	defer r.Body.Close()
 
